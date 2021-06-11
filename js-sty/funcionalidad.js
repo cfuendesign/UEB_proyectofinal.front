44dc39663426d659192d96737748bd4dfc820289
JulianSilva-coder <JulianSilva-coder@github.com>
2021-06-10 23:40:26 -0500

    pera

--- js-sty/funcionalidad.js
+++ js-sty/funcionalidad.js
@@ -1,11 +1,12 @@
 Function
-function pagina() {
-    location.href = "registro.html";
-}
-Function
 function atras() {
     location.href = "index.html";
 }
+Function
+function pagina() {
+    location.href = "registro.html";
+}
+
 Function
 function propietario(){
     location.href = "Panel-Propietario.html";
