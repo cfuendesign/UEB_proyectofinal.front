1c50fb0ac661ca5f35e0f7246e0b5a3412f7b110
cfuen <SapoPerroGitHub>
2021-06-17 18:14:59 -0500

    Esto más que feature es fix, intentaré agregar por mi cuenta las funciones de Fetch para que el Front se comunique con el Restful

--- js-sty/funcionalidad.js
+++ js-sty/funcionalidad.js
@@ -1,3 +1,14 @@
+
+//Variable que indica el estado de la página (Cargada/No Cargada)
+var isPageLoaded = 0;
+
+//Event listener que se engatilla cuando se carga la página e indica, mediante la variable isPageLoaded
+//que la página está cargada
+document.addEventListener("DOMContentLoaded", function (event) {
+    isPageLoaded = 1;
+});
+
+//Condición que impide que se ejecute el evento onchange hasta que no se 
 if (isPageLoaded = 1) {
     document.getElementById('file').onchange = function (e) {
         let reader = new FileReader();
@@ -16,53 +27,59 @@ if (isPageLoaded = 1) {
         }
     }
 }
-var isPageLoaded = 0;
-document.addEventListener("DOMContentLoaded", function (event) {
-    isPageLoaded = 1;
-});
 
+//Para cuando te encuentres en una página que no es el index, te manda al index
 function atras() {
     location.href = "index.html";
 }
 
-
+//???
 function pagina() {
     location.href = "registro.html";
 }
 
-
+//Ventana del Propietario
 function propietario() {
-    location.href = "Panel-Propietario.html";
+    location.href = "Ventana-Propietario.html";
 }
 
-function veterinaria() {
-    location.href = "Ventana-veterinaria.html";
-}
-
-function atras1() {
-    location.href = "index.html"
-}
-
-function Alcaldia() {
-    location.href = "Alcaldia.html";
-}
-
+//Abre el pop-up de creación de mascota?
 function Guardar() {
     document.getElementById("ventana-pop").style.display = "block";
 }
 
+//Cierra el pop-up de creación de mascota?
 function cerrar() {
     document.getElementById("ventana-pop").style.display = "none";
 }
 
+//Ventana de la Veterinaria
+function veterinaria() {
+    location.href = "Ventana-Veterinaria.html";
+}
+
+//Ventana de la Alcaldía
+function Alcaldia() {
+    location.href = "Ventana-Alcaldia.html";
+}
+
+//??? Un clon de atras, aparentemente
+function atras1() {
+    location.href = "index.html"
+}
+
+//El formulario del propietario en forma de *constante*. Debería ser una variable (var/let)
 const formpropier = document.forms['propietario-form'];
 
+//Función que se engatilla cuando el usuario envía el formulario
 formpropier.onsubmit = (event) => {
     event.preventDefault();
     console.log(formDataJSON());
     formpropier.reset();
 };
 
+//TODO / Fix - This is dumb
+//Función que toma lo que se ha ingresado en el formulario del propietario y lo convierte en JSON
 function formDataJSON() {
     const user = {};
     Array.from(formpropier.elements).forEach(element => {
