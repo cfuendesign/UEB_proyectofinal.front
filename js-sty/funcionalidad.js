34211d2a762a712b7d7350265f65d8450a8569ac
JulianSilva-coder <JulianSilva-coder@github.com>
2021-06-16 22:13:50 -0500

    Prueba datos

--- js-sty/funcionalidad.js
+++ js-sty/funcionalidad.js
@@ -1,56 +1,73 @@
-if (isPageLoaded = 1){
-    document.getElementById('file').onchange=function(e){
+if (isPageLoaded = 1) {
+    document.getElementById('file').onchange = function (e) {
         let reader = new FileReader();
         reader.readAsDataURL(e.target.files[0]);
-        reader.onload=function(){
+        reader.onload = function () {
             let preview = document.getElementById('preview');
-            canvas=document.createElement('canvas');
-            canvas.width=400; canvas.height=400;
-            preview.innerHTML='';
+            canvas = document.createElement('canvas');
+            canvas.width = 400;
+            canvas.height = 400;
+            preview.innerHTML = '';
             preview.append(canvas);
             let canvctrl = canvas.getContext("2d");
-            image = new Image(); image.src = reader.result;
+            image = new Image();
+            image.src = reader.result;
             image.onload = () => canvctrl.drawImage(image, 0, 0, 400, 400);
         }
     }
 }
 var isPageLoaded = 0;
-document.addEventListener("DOMContentLoaded", function(event) {
+document.addEventListener("DOMContentLoaded", function (event) {
     isPageLoaded = 1;
 });
 Function
+
 function atras() {
     location.href = "index.html";
 }
+
 Function
+
 function pagina() {
     location.href = "registro.html";
 }
 
 Function
-function propietario(){
+
+function propietario() {
     location.href = "Panel-Propietario.html";
 }
+
 Function
-function veterinaria(){
+
+function veterinaria() {
     location.href = "Ventana-veterinaria.html";
 }
+
 Function
-function atras1(){
+
+function atras1() {
     location.href = "index.html"
 }
+
 Function
-function Alcaldia(){
+
+function Alcaldia() {
     location.href = "Alcaldia.html";
 }
+
 Function
-function Guardar(){
-    document.getElementById("ventana-pop").style.display="block";
+
+function Guardar() {
+    document.getElementById("ventana-pop").style.display = "block";
 }
+
 Function
-function cerrar(){
-    document.getElementById("ventana-pop").style.display="none";
+
+function cerrar() {
+    document.getElementById("ventana-pop").style.display = "none";
 }
+
 const formpropier = document.forms['propietario-form'];
 
 formpropier.onsubmit = (event) => {
@@ -58,10 +75,10 @@ formpropier.onsubmit = (event) => {
     console.log(formDataJSON());
 };
 
-function formDataJSON(){
-const user = {};
-Array.from(formpropier.elements).forEach(element => {
-    if(element.name) user[element.name] = element.value;
-});
-return user;
+function formDataJSON() {
+    const user = {};
+    Array.from(formpropier.elements).forEach(element => {
+        if (element.name) user[element.name] = element.value;
+    });
+    return user;
 }
