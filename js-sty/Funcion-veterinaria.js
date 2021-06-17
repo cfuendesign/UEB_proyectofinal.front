591f4b0ee899e331d6ebb65006128bd0a75327f7
JulianSilva-coder <JulianSilva-coder@github.com>
2021-06-17 13:43:03 -0500

    prueba datos

--- js-sty/Funcion-veterinaria.js
+++ js-sty/Funcion-veterinaria.js
@@ -9,16 +9,3 @@ function Guardar() {
 function cerrar() {
     document.getElementById("ventana-pop").style.display = "none";
 }
-const formvetter = document.forms['veterinarian-form'];
-formvetter.onsubmit = (event) => {
-    event.preventDefault();
-    console.log(formDataJSON());
-};
-
-function formDataJSON() {
-    const user1 = {};
-    Array.from(formvetter.elements).forEach(element => {
-        if (element.name) user1[element.name] = element.value;
-    });
-    return user1;
-}
