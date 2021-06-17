8c2cf4d41b417e512c91397441676df4341ae06f
JulianSilva-coder <JulianSilva-coder@github.com>
2021-06-17 01:00:13 -0500

    Prueba veterinaria

--- js-sty/funcionalidad.js
+++ js-sty/funcionalidad.js
@@ -20,50 +20,34 @@ var isPageLoaded = 0;
 document.addEventListener("DOMContentLoaded", function (event) {
     isPageLoaded = 1;
 });
-Function
 
 function atras() {
     location.href = "index.html";
 }
 
-Function
 
 function pagina() {
     location.href = "registro.html";
 }
 
-Function
 
 function propietario() {
     location.href = "Panel-Propietario.html";
 }
 
-Function
-
 function veterinaria() {
     location.href = "Ventana-veterinaria.html";
 }
-
-Function
-
 function atras1() {
     location.href = "index.html"
 }
-
-Function
-
 function Alcaldia() {
     location.href = "Alcaldia.html";
 }
 
-Function
-
 function Guardar() {
     document.getElementById("ventana-pop").style.display = "block";
 }
-
-Function
-
 function cerrar() {
     document.getElementById("ventana-pop").style.display = "none";
 }
